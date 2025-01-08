f4b63615df740b1d36f557516dda9dc4c49c8ff4
Max <kazakovmvmvm@gmail.com>
2025-01-09 00:24:25 +0300

    Рассылка писем

---
+++ module_4_2.py
@@ -0,0 +1,26 @@
+import re
+
+
+def send_mail(message, recipient, sender='Kazakovmvmvm@gmail.com'):
+    def is_valid_email(email):
+        email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'
+        return re.match(email_regex, email)
+
+    if not is_valid_email(sender):
+        print('Ошибка: Некорректный email адрес')
+        return
+
+    if not is_valid_email(recipient):
+        print('Ошибка: Некорректный email адрес')
+        return
+
+    if sender == recipient:
+        print('Ошибка: вы пытаетесь отправить сообщение себе')
+        return
+
+    if sender == 'Kazakovmvmvm@gmail.com':
+        print(f'Внимание: Этот: {sender} адрес используется по умолчанию')
+
+    print(f'Письмо отправляет: {sender}, Получатель: {recipient}: {message}')
+
+send_mail('День добрый', 'poluchatrl@mail.ru', 'otpravitrl@mail.ru')
